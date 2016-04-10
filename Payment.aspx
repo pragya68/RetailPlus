9355214fc7fd7f522507dcbacf019dfffa30a181
Sushant Bansal <1star.sushant@gmail.com>
2016-04-10 21:21:15 +0530

    Update
    Added Payment Options and Shipping Address

---
+++ Payment.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GuestMasterPage.master" AutoEventWireup="true" CodeFile="Payment.aspx.cs" Inherits="Payment" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="login">
+        <div class="login-top">
+		<h1 style="text-transform:none";>Enter Payment Details</h1>
+            <div>
+            <asp:HiddenField ID="Payx" runat="server" />
+            <asp:RadioButton ID="Cash" Text="<img src=css/images/Cash600x400png.png height=100px width=150px/>" runat="server" AutoPostBack="true" OnCheckedChanged="Cash_CheckedChanged" GroupName="PAyment" />
+            <asp:RadioButton ID="Card" Text="<img src=css/images/Payment600x400.png height=100px width=150px />" runat="server" AutoPostBack="true" OnCheckedChanged="Card_CheckedChanged" GroupName="PAyment" />
+            <br />
+             </div>
+            <br />
+            <br />
+        <asp:TextBox ID="CardNo" runat="server" placeholder="Card Number (not required for Cash on Delivery)"></asp:TextBox>
+            <br />
+        <asp:TextBox ID="BankName" runat="server" placeholder="Bank Name (not required for Cash on Delivery) "></asp:TextBox>
+	    </div>
+        <div class="forgot" style="margin-top:75px;">
+            <asp:Button ID="PayNow" runat="server" Text="Proceed" OnClick="PayNow_Click" />
+        </div>
+        
+	</div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="footer">
+                <div class="left_footer">
+                    <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="#">contact us</a>
+                </div>
+                <div class="center_footer">
+                        
+                </div>
+                <div class="right_footer">
+                <img src="css/images/websitebasket.png" alt="" width="100" height="100" />
+                 </div>
+            </div>
+</asp:Content>
+
---
+++ Payment.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Payment : System.Web.UI.Page
+{
+    //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pragya\Documents\SEM_4\DBMS_Project\RetailPlus\App_Data\Database.mdf;Integrated Security=True");
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Sushant\Documents\GitHub\RetailPlus\App_Data\Database.mdf;Integrated Security=True");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //PaymentType.Text = String.Empty;
+    }
+
+    protected void PayNow_Click(object sender, EventArgs e)
+    {
+        pay();
+    }
+
+    void pay()
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("Insert into PaymentDetails Values('"+ Payx.Value +"','"+ CardNo.Text +"','" + BankName.Text + "') ", con);
+        int i = cmd.ExecuteNonQuery();
+        if (i > 0)
+        {
+            addToOrder();
+        }
+    }
+
+       
+    protected void Cash_CheckedChanged(object sender, EventArgs e)
+    {
+        if (Cash.Checked)
+            Payx.Value = "COD"; 
+    }
+
+    protected void Card_CheckedChanged(object sender, EventArgs e)
+    {
+        if (Cash.Checked)
+            Payx.Value = "Card";
+    }
+
+    void addToOrder()
+    {
+        Random R = new Random();
+        double D = R.Next(1, 4);
+        SqlCommand cmd = new SqlCommand("Insert into Orders Values('"+Session["Oid"]+"','"+Session["id"]+",@OrderDate,@OrderTime,@ShippingDate,'"+"N"+"',"+getPaymentID()+",", con);
+        cmd.Parameters.Add("@OrderDate", SqlDbType.Date).Value = DateTime.Now.Date;
+        cmd.Parameters.Add("@OrderTime", SqlDbType.Date).Value = DateTime.Now.TimeOfDay;
+        cmd.Parameters.Add("@ShippingDate", SqlDbType.Date).Value = DateTime.Now.Date.AddDays(D);
+        int i = cmd.ExecuteNonQuery();
+        if (i > 0)
+        {
+                
+        }
+    }
+
+    int getPaymentID()
+    {
+        int value = 0 ;
+        SqlCommand nCmd = new SqlCommand("Select TOP 1 * from PaymentDetails order by PaymentID desc", con);
+        SqlDataAdapter nDa = new SqlDataAdapter(nCmd);
+        DataTable nDt = new DataTable();
+        nDa.Fill(nDt);
+        if (nDt.Rows.Count > 0)
+        {
+         value = Convert.ToInt32(nDt.Rows[0]["PaymentID"].ToString());
+        }
+        return value;
+    }
+}
